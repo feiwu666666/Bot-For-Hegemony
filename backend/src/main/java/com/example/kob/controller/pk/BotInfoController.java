29e50066d92653f97e4658909aa867edee639a8c
Cyan_Breeze <3381329086@qq.com>
2022-08-14 21:45:39 +0800

    基本骨架搭建完成

--- backend/src/main/java/com/example/kob/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/example/kob/controller/pk/BotInfoController.java
@@ -3,8 +3,10 @@ package com.example.kob.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.LinkedList;
+import java.util.Map;
 
 // 前后端分离  向服务端传数据时用ResController 注解
 
@@ -13,12 +15,12 @@ import java.util.LinkedList;
 public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
-    public List<String> getBotInfo(){
-        List<String> list = new LinkedList<>();
-        list.add("sword");
-        list.add("tiger");
-        list.add("apple");
-        return list;
+    public Map<String,String > getBotInfo(){
+        List<Map<String,String>> list = new LinkedList<>();
+        Map<String,String> bot1 = new HashMap<>();
+        bot1.put("name","Jack");
+        bot1.put("rating","1003");
+        return bot1;
     }
 
 }
