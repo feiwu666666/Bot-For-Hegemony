9bba31984153ae5b106380bbae95beadfa7c2ce1
Cyan_Breeze <3381329086@qq.com>
2022-08-17 14:13:06 +0800

    实现了导航栏和地图

---
+++ web/src/assets/scripts/GameMap.js
@@ -0,0 +1,120 @@
+import { AcGameObject } from "./AcGameObject";
+import { Wall } from "./Wall";
+
+
+export class GameMap extends AcGameObject{
+    constructor (ctx,parent){ // ctx画布 , parent 画布的父元素
+        super();
+
+        this.ctx = ctx;
+        this.parent = parent; // 父元素
+        this.L = 0; // 一个单位的长度
+        this.cols = 13;
+        this.rows = 13;
+        this.inner_walls_count = 20;  // 假设地图中有20个障碍物
+        this.walls = []; //墙体
+
+    }
+    check_connectivity (g ,sx ,sy , zx , zy){
+        // 使用Flood Fill 算法寻找连通块
+        if(sx == zx && sy == zy) return true; // 如果起点和终点重合 即连通
+        g[sx][sy] = true; // 标记为走过的路
+        let fx = [-1,0,1,0]; // 四个方向的偏移量
+        let fy = [0,1,0,-1];
+        for(let i = 0 ;i < 4; i++){
+            let x = sx + fx[i]; // 新的起点
+            let y = sy + fy[i];
+            if(!g[x][y] && this.check_connectivity(g,x,y,zx,zy)){ // 如果碰到墙体或者该格走过 就不能再走了
+                return true;
+            }
+
+        }
+        return false;
+
+    }
+    create_walls(){
+          const g = [];
+          // 将所有格子都初始化
+          for(let r = 0; r < this.rows ; r++){
+            g[r] = [];
+            for(let c = 0;c < this.cols ;c++){
+                g[r][c] = false;
+            }
+          }
+          // 将四周一圈全部设为墙，封住地图
+          for(let r = 0; r< this.rows; r++){
+                g[r][0] = g[r][this.cols - 1] = true;
+            }
+            for(let c = 0; c < this.cols ; c ++){
+                g[0][c]  =g[this.rows - 1][c] = true;
+            }
+          //  创建随机障碍物
+          // 为了展现公平性 , 创建墙体时采用中心对称的方法，双方墙体中心对称，考验双方bot的机动性
+          for(let i = 0 ;i < this.inner_walls_count / 2 ; i++){  // 每次放两个，所以i对半取
+            for(let j = 0;j < 100000; j++){
+                let r = parseInt(Math.random() * this.rows); // random函数返回[0,1)中的随机数  乘上行列数取整，返回[0,cols)的随机值
+                let c = parseInt(Math.random() * this.cols);
+
+                if(g[r][c] || g[c][r])  // 如果当前格子存在墙体，则进行下一次随机
+                continue;
+                else if(r == this.rows - 2 && c == 1) // 如果随即墙体刷在四周，也进行下一次随机
+                continue;
+                else if(r == 1 && c ==this.cols - 2)
+                continue;
+                
+                g[r][c] = g[c][r] = true; // 设当前随机墙体为障碍物，并将其中心对称格子同样设为障碍物
+                break;
+                
+            }
+          }
+
+
+          
+
+          let copy_g = JSON.parse(JSON.stringify(g));
+          // 检查是否可以连通
+          if(!this.check_connectivity(copy_g,this.rows-2 , 1, 1,this.cols - 2)) return false;
+
+          // 绘制墙体
+          for(let r = 0;r < this.rows; r++){
+            for(let c = 0; c < this.cols; c++){
+                if(g[r][c]){
+                    this.walls.push(new Wall(r,c,this));
+                }
+            }
+          }
+
+          return true;  // 如果创建墙体和障碍物成功  返回true
+
+    }
+    start(){
+        for(let i = 0;i < 100000;i ++){
+            if(this.create_walls()) break;
+        }
+    }
+    update_size(){
+        // min中的值可能为浮点型，会导致每个墙体之间会出现缝隙，可以用parseInt取整，去除缝隙
+        this.L = parseInt(Math.min(this.parent.clientWidth/this.cols,this.parent.clientHeight/this.rows));
+        this.ctx.canvas.width = this.L * this.cols;
+        this.ctx.canvas.height = this.L * this.rows;
+    }
+    update(){
+        this.update_size();
+        this.render();
+    }
+    render(){
+        const even_color = "#AAD751",old_color = "#A2D149";
+        for(let r = 0; r < this.rows; r++){
+
+            for(let c = 0; c<this.cols; c++){
+                if((r+c) % 2 == 0){
+                    this.ctx.fillStyle = even_color;
+                }else{
+                    this.ctx.fillStyle = old_color;
+                }
+                this.ctx.fillRect(this.L * c,this.L * r,this.L,this.L);
+            }
+        }
+    }
+
+}
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,11 +1,49 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+import PkIndexView from '../views/pk/PkIndexView'
+import NotFound from '../views/error/NotFound'
+import RanklistIndexView from '../views/ranklist/RanklistIndexView'
+import RecordIndexView from '../views/record/RecordIndexView'
+import UserBotIndexView from '../views/user/bot/UserBotIndexView'
 
 const routes = [
-  
+  {
+    path:'/',
+    name:"home",
+    redirect:"/pk/"
+  },
+  {
+    path:"/pk/",
+    name:"pk_index",
+    component:PkIndexView
+  },
+  {
+    path:"/record/",
+    name:"record_index",
+    component:RecordIndexView
+  },
+  {
+    path:"/ranklist/",
+    name:"ranklist_index",
+    component:RanklistIndexView
+  },
+  {
+    path:"/user/bot/",
+    name:"user_bot_index",
+    component:UserBotIndexView
+  },
+  {
+    path:"/404/",
+    name:"404_index",
+    component:NotFound
+  },
+  {
+    path:"/:catchAll(.*)",
+    redirect:'/404/'
+  }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
